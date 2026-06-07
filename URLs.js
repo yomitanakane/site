670885f0daf1448a3eb3206d0d4df0d22e7e584b
読谷あかね <akaneyomitan@gmail.com>
2026-06-07 13:47:08 +0900

    Add files via upload

---
+++ URLs.js
@@ -0,0 +1,26 @@
+/**
+ * URLs.js
+ * CONTACTページの「URLs」セクションに表示するリンク一覧を管理します。
+ * portfolio.html と同じディレクトリに置いてください。
+ */
+
+const URL_ITEMS = [
+  { label: "YouTube",       url: "https://www.youtube.com/@yomitanakane" },
+  { label: "Niconico",      url: "https://www.nicovideo.jp/user/118174928" },
+  { label: "BOOTH",         url: "https://yomitan-akane.booth.pm/" },
+  { label: "X",             url: "https://x.com/yomitanakane" },
+  { label: "CTC RECORDS",   url: "https://ctctyo.com/collections/ctcrecored-popup" },
+  { label: "Spotify",       url: "https://open.spotify.com/intl-ja/artist/6hmC01ibmH7YxREDOvLaqE?si=8kzOjn74Tuq-foWnw4eJPQ" },
+  { label: "Discord",       url: "https://discord.com/invite/bVskVWpy5M" },
+  { label: "bilibili",      url: "https://space.bilibili.com/3493282275395793" },
+  { label: "TuneCoreJapan", url: "https://www.tunecore.co.jp/artists/Yomitan-Akane" },
+  { label: "Apple Music",   url: "https://music.apple.com/jp/artist/%E8%AA%AD%E8%B0%B7%E3%81%82%E3%81%8B%E3%81%AD/1691994334" },
+  { label: "LINEスタンプ",  url: "https://store.line.me/stickershop/author/4734224/ja" },
+  { label: "SUZURI",        url: "https://suzuri.jp/yomitan" },
+  { label: "Blog",          url: "https://yomitanakane.hatenablog.com/" },
+  { label: "FEAT",          url: "https://feat.kurogo.studio/yomitanakane" },
+  { label: "Bluesky",       url: "https://bsky.app/profile/yomitanakane.bsky.social" },
+  { label: "YouTube Sub",   url: "https://www.youtube.com/@YomitanSub" },
+  { label: "X Sub",         url: "https://x.com/yomitansub" },
+  { label: "旧HP",          url: "https://yomitanakane.myportfolio.com" },
+];
