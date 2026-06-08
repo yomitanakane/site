b60620c02de3db9f96172647669397787d5b8c64
読谷あかね <akaneyomitan@gmail.com>
2026-06-08 14:47:26 +0900

    Update URLs.js

--- URLs.js
+++ URLs.js
@@ -15,6 +15,7 @@ const URL_ITEMS = [
   { label: "bilibili",      url: "https://space.bilibili.com/3493282275395793" },
   { label: "TuneCoreJapan", url: "https://www.tunecore.co.jp/artists/Yomitan-Akane" },
   { label: "Apple Music",   url: "https://music.apple.com/jp/artist/%E8%AA%AD%E8%B0%B7%E3%81%82%E3%81%8B%E3%81%AD/1691994334" },
+  { label: "YouTube Music",   url: "https://music.youtube.com/@yomitanakane" },
   { label: "LINEスタンプ",  url: "https://store.line.me/stickershop/author/4734224/ja" },
   { label: "SUZURI",        url: "https://suzuri.jp/yomitan" },
   { label: "Blog",          url: "https://yomitanakane.hatenablog.com/" },
