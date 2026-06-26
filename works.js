fe23c87743e86376c04fa8928af30d23b01ce1a1
読谷あかね <akaneyomitan@gmail.com>
2026-06-26 19:18:45 +0900

    Update works.js

--- works.js
+++ works.js
@@ -6,6 +6,7 @@
 ========================================================================== */
 const WORKS = [
   // ---------------------------------------------------------------- 2026
+  { title: "雨良『ぽい』", tag: "映像", year:"2026", ytId: "bNPzF89addE", img:"images/Poi.png"},
 　{ title: "已己巳己 / 重音テト", tag: "楽曲", year:"2026", url: "https://youtu.be/SNlR_U4CoPA?si=DjziOwdG6DqMp52x", img:"images/Ikomiki.png"},
   { title: "読谷あかねのリミナルスペースお散歩日記", tag: "その他", year:"2026", url: "https://youtu.be/B-nUHn-z9pk", img:"images/Liminal.png"},
   { title: "Vocation Chiba Vol.115", tag: "出演", year:"2026", url: "https://twipla.jp/events/727217", img:"images/Vocachiba.png"},
